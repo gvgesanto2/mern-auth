e2443af2931c6e14f9237ff2ca9817a1ec3da194
Gabriel Santo <gvgesanto2@gmail.com>
2020-07-02 00:27:17 -0300

    adding some middleware

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config("config/config.env");
 
 const app = express();
 
 // import routes
 const authRoutes = require("./routes/auth.routes");
+
+// app middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+    })
+  );
+}
 
 // middleware
 app.use("/api", authRoutes);
