3dfbd4c54adf87cca69f30f6244f7894ffe02fd0
Gabriel Santo <gvgesanto2@gmail.com>
2020-07-07 18:20:44 -0300

    connecting to the mongoDB database

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-require("dotenv").config("config/config.env");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Load env vars
+require('dotenv').config({ path: 'config/config.env' });
 
 const app = express();
 
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.log('DB CONNECTION ERROR: ', err));
+
 // import routes
-const authRoutes = require("./routes/auth.routes");
+const authRoutes = require('./routes/auth.routes');
 
 // app middlewares
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   app.use(
     cors({
-      origin: process.env.CLIENT_URL,
+      origin: process.env.CLIENT_URL
     })
   );
 }
 
 // middleware
-app.use("/api", authRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.port || 8000;
+
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);
 });
